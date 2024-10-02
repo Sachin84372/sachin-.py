1bbf527875ef8cd8ffaaece14cd54d4141d83bfe
sachin kumar <166626832+Sachin84372@users.noreply.github.com>
2024-10-03 00:44:32 +0530

    Add files via upload

---
+++ while loop18.py
@@ -0,0 +1,38 @@
+                                                   #day 18#
+                                    # using the  while loop in the python 
+
+#  #using the simple code by using {the while loop  in INCREMENT the python }
+
+number=0
+while (number <=5):      # we give the condition there that {i less thabn or equal t ot he 5}
+    print(number)        # help to print the number
+    number=(number+1)    # it help to increment  by {+1 in the number}
+      
+#       ###END THIS IS THE BAISC WHILE LOOP ###
+
+ #using the simple code by using {the while loop  in  DECRIMENT the python }
+
+sachin=5              # sachin =5 say the loop start from  the 5
+while(sachin>-2):     # while loop say the condition wili go to the -2 {5,4,3,2,1,0,-1,}
+    print(sachin)     # print the sachin (no.) one by one 
+    sachin=(sachin-1) # it decrese the no. one by one
+
+
+# #taking the input from the user  {for know more deeply about the while loop}#
+
+count =int (input("enter the number:"))
+
+while (count <=30):
+    count =int (input("enter the number:"))
+    print(count)
+print ("the program is done on the  while loop")
+#      ### END OF WHILE LOOP BY TAKING USER INPUT ###
+
+# #taking the input from the user  {for know more deeply about the while loop} also using the else condition in this#
+
+num=int(input("enter the  higher no. :"))
+while(num<99):
+    num=int(input("enter your number agin : "))
+    num=(num)
+else:
+ print ("well done you get out from the loop ")
